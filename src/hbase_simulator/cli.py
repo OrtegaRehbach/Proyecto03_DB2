32c030d4ccf63e2e3b9a8ea4ac7d8f02c986617c
Alejandro Ortega <ort18248@uvg.edu.gt>
2024-05-29 12:44:45 -0600

    Fixed: Truncate checking table incorrectly

--- src/hbase_simulator/cli.py
+++ src/hbase_simulator/cli.py
@@ -162,7 +162,7 @@ def run_cli():
                     table_name = args[1]
                     if table_name in simulator.tables:
                         table = simulator.tables[table_name]
-                        if not table.enabled:
+                        if table.enabled:
                             # Deshabilitar la tabla antes de truncarla
                             print("Truncating 'one' table: \n  - Disabling table... ")
                             table.disable()
